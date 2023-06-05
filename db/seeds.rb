d179ed9b1c20f93fa2c537c77613d5cfc7800e6c
ram1117 <ramkumark1@outlook.com>
2023-06-05 23:08:36 +0530

    create quote index api endpoint, fix rubocop errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Quote.destroy_all
+
+Quote.create([
+               { name: 'Hello User, This is Rails App with React in the same project' },
+               { name: 'Hallo Benutzer, dies ist eine Rails-App mit React im selben Projekt' },
+               { name: 'Hallo gebruiker, dit is de Rails-app met React in hetzelfde project' },
+               { name: 'Hola usuario, esta es la aplicación Rails con React en el mismo proyecto' },
+               { name: 'Hej bruger, dette er Rails App med React i samme projekt' }
+             ])
